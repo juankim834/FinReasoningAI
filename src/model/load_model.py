ed8ac7cf55d8c5a268cd765623fb5b28e0d84972
juankim834 <juankim834@gmail.com>
2026-05-04 02:03:44 -0400

    fix bf16 issue

--- src/model/load_model.py
+++ src/model/load_model.py
@@ -97,7 +97,7 @@ def enable_safe_bf16_causal_mask_patch() -> bool:
 DEFAULT_BNB_CONFIG = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_compute_dtype=torch.bfloat16,
     bnb_4bit_use_double_quant=True,
 )
 
