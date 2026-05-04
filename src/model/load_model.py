55aa8322cb993cf0dc44908f7ed8f333f315f477
juankim834 <juankim834@gmail.com>
2026-05-04 01:49:56 -0400

    fix bf16 issue

--- src/model/load_model.py
+++ src/model/load_model.py
@@ -41,7 +41,7 @@ def get_preferred_torch_dtype() -> torch.dtype:
 DEFAULT_BNB_CONFIG = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=get_preferred_torch_dtype(),
+    bnb_4bit_compute_dtype=torch.float16,
     bnb_4bit_use_double_quant=True,
 )
 
